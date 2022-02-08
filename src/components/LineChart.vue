bc0e7ef2f78c633a6625795ddf207e23f8baea5a
Ahmad <ahmadasadi37@gmail.com>
2022-02-08 15:07:16 +0100

    edit graph color styling

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -14,14 +14,19 @@ export default {
       type: Object,
     },
     chartColors: {
-        type: Object,
-    }
+      type: Object,
+    },
   },
   mounted() {
-    const dates = this.chartData.map(d => d.date).reverse();
-    const totals = this.chartData.map(d => d.total).reverse();
+    const dates = this.chartData.map((d) => d.date).reverse();
+    const totals = this.chartData.map((d) => d.total).reverse();
 
-    const {borderColor, pointBorderColor, pointBackgroundColor, backgroundColor} = this.chartColors;
+    const {
+      borderColor,
+      pointBorderColor,
+      pointBackgroundColor,
+      backgroundColor,
+    } = this.chartColors;
 
     this.renderChart(
       {
